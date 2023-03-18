fd4ba914565f0c983f184fc57ebd3d61d325dfc1
sadathimel <sadat.himel@gmail.com>
2023-03-18 17:48:20 +0600

    updated

---
+++ banglalink/v4/v1/script.js
@@ -0,0 +1,68 @@
+let currentCounter = 0;
+let increment = 893456;
+
+let targetValue = 40000000;
+
+updateCounter()
+
+
+function updateCounter() {
+  if (currentCounter < targetValue) {
+    currentCounter += increment;
+    if (currentCounter > targetValue) {
+      currentCounter = targetValue;
+    }
+    document.querySelector('.ppm_numberCounter').innerHTML = translateNumToBangla(currentCounter);
+    setTimeout(() => {
+      updateCounter()
+    }, 20);
+  }
+}
+
+function translateNumToBangla(num) {
+  banglaNum = ['০', '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯']
+  banglaNumText = '';
+  numText = num.toString()
+  for (let c of numText) {
+    banglaNumText += banglaNum[parseInt(c)];
+  }
+  return banglaNumText;
+}
+
+
+gsap.timeline()
+  .to('.ppm_orangeOverlay', { opacity: 0, duration: 4 })
+  .to(['.ppm_blankText', '.ppm_numberCounter'], { opacity: 0, display: 'none' }, '>.5')
+  .to('.ppm_fullText', { opacity: 1, display: 'block' }, '<')
+  .to('.ppm_fullText', {
+    scaleX: 1.05, scaleY: 1.05, duration: .4, repeat: 1, yoyo: true, ease: 'none'
+  }, '>.7')
+  .to('.ppm_fullText', {
+    scaleX: 1, scaleY: 1, duration: .4,
+    onComplete: repeatAnimation, ease: 'none'
+  },'>')
+
+
+function repeatAnimation() {
+  setTimeout(() => {
+    currentCounter = 0;
+    document.querySelector('.ppm_numberCounter').innerHTML='০০০০০';
+    gsap.to('.ppm_fullText', { opacity: 0, display: 'none', delay: .1 })
+    gsap.to(['.ppm_blankText', '.ppm_numberCounter', '.ppm_orangeOverlay'], { opacity: 1, display: 'block', onComplete: secondStep })
+  }, 1000);
+}
+
+function secondStep() {
+  updateCounter()
+  gsap.timeline()
+    .to('.ppm_orangeOverlay', { opacity: 0, display: 'none', duration: 4 })
+    .to(['.ppm_blankText', '.ppm_numberCounter'], { opacity: 0, display: 'none' }, '>.5')
+    .to('.ppm_fullText', { opacity: 1, display: 'block' }, '<')
+    .to('.ppm_fullText', {
+      scaleX: 1.05, scaleY: 1.05, duration: .4, repeat: 1, yoyo: true, ease: 'none'
+    }, '>.7')
+    .to('.ppm_fullText', {
+      scaleX: 1, scaleY: 1, duration: .4,
+      onComplete: repeatAnimation, ease: 'none'
+    },'>')
+}
